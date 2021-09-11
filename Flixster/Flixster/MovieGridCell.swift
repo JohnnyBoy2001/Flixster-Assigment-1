62397c99baa71d4823072932795bf0c16c67ac1c
JohnnyBoy2001 <johnnytowwez@gmail.com>
2021-09-11 16:45:51 -0400

    Part 2

---
+++ Flixster/Flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by wikiwoo on 9/11/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
